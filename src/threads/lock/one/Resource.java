e04dc6e25e431587602576442da5b4cffc45266b
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-02-14 17:42:03 +0530

    Create Resource.java

---
+++ src/threads/lock/one/Resource.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threads.lock.one;
+
+
+public abstract class Resource {
+    
+    public void resource() throws Exception{
+        
+    }
+    
+    public void resource(int n) throws Exception{
+        
+    }
+}
