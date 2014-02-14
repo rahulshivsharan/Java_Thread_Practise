29353e3ea32a67329cc9ec964bde0660e92487bd
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-02-14 17:37:32 +0530

    Create ResourceOne.java

---
+++ src/threads/deadlock/ResourceOne.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package threads.deadlock;
+
+
+public class ResourceOne {
+    
+    public synchronized void callOne(ResourceTwo r2) throws Exception{
+        Thread t = Thread.currentThread();    
+        for(int x=1;x <=3; x++){
+            System.out.println(t.getName()+", callOne "+x);
+            if(x == 2){
+                r2.callTwo();
+            }
+        }
+    }
+    
+    public synchronized void pollTwo() throws Exception{
+        Thread t = Thread.currentThread();    
+        for(int x=1;x <=3; x++){
+            System.out.println(t.getName()+", pollTwo "+x);            
+        }
+    }
+}
