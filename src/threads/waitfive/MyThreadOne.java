d86eb028b1667e7e5707e1be7e10fa27ae08f093
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-03-31 16:58:32 +0530

    The output of the above file is
    run:
    JACK Acess... 
    JACK : 1
    JACK : 2
    JACK : 3
    JACK : 4
    JILL Acess... 
    JILL : 1
    JILL : 2
    JILL : 3
    JILL : 4
    JILL : 5
    JACK : 5
    JACK : 6
    JACK : 7
    JACK : 8
    JACK : 9
    JACK : 10
    JILL : 6
    JILL : 7
    JILL : 8
    JILL : 9
    JILL : 10
    BUILD SUCCESSFUL (total time: 0 seconds)

---
+++ src/threads/waitfive/MyThreadOne.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package threads.waitFive;
+
+
+public class MyThreadOne extends Thread{
+    
+    private SharedResource res;
+    private  int num;
+    
+    public MyThreadOne(String name,SharedResource res,int num){
+        this.setName(name);
+        this.res = res;
+        this.num = num;
+    }
+    
+    @Override
+    public void run(){
+        try{
+            res.counter(num);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String [] args){
+        try{
+            SharedResource res = new SharedResource();
+            MyThreadOne t1 = new MyThreadOne("JACK",res,4);
+            MyThreadOne t2 = new MyThreadOne("JILL",res,5);
+            
+            t1.start();
+            t2.start();
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
