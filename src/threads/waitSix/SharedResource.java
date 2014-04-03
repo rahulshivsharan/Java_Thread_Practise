bd0b9b1cb28f220ee6759d1f0db2348838650f41
Rahul Shivsharan <rahulshivsharan@gmail.com>
2014-04-03 16:13:25 +0530

    Create SharedResource.java

---
+++ src/threads/waitSix/SharedResource.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package threads.waitSix;
+
+
+public class SharedResource {
+    private volatile int counter = 1;
+    
+    public synchronized boolean printNum(int num,boolean isLastNum) throws Exception{
+        boolean printed = false; 
+        if(num == counter){            
+            System.out.println(Thread.currentThread().getName()+" "+num);
+            counter++;
+            printed = true;
+            if(isLastNum){
+                this.notifyAll();
+            }
+        }else{
+            printed = false;
+            this.notifyAll();
+            this.wait();            
+        }
+        
+        return printed;
+    }
+
+    
+}
